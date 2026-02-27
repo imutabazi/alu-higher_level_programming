1ee4ae28c4e1d9795015a099f61116a0e7d1ae67
root <i.mutabazi@alustudent.com>
2026-02-27 08:48:21 -0800

    test

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import sys
+
 if __name__ == "__main__":
     num_args = len(sys.argv) - 1
     if num_args == 0:
@@ -8,6 +9,6 @@ if __name__ == "__main__":
         print("1 argument:")
     else:
         print("{} arguments:".format(num_args))
-    
+
     for i in range(1, num_args + 1):
         print("{}: {}".format(i, sys.argv[i]))
